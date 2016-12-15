43b80a1a68c38ba6e16797e3d6c9c220bdd6c44d
zhangxin <nzt48.xin@gmail.com>
2016-12-15 11:16:01 +0800

    修复部分问题

--- presistence-project/persisitence-impl/src/main/java/com/a/eye/skywalking/test/persistence/mysql/MySqlServiceManager.java
+++ presistence-project/persisitence-impl/src/main/java/com/a/eye/skywalking/test/persistence/mysql/MySqlServiceManager.java
@@ -27,20 +27,21 @@ public class MySqlServiceManager {
     private static final String TABLE_NAME = "CACHE_TABLE";
 
     private static final String QUERY_SQL = "SELECT id, CACHE_KEY, CACHE_VALUE FROM CACHE_TABLE WHERE CACHE_KEY= :cacheKey";
-    private static final String TABLE_CHECK_SQL = "SELECT * FROM information_schema.tables WHERE table_schema = :table_schema AND table_name = :tableName LIMIT 1;";
-    private static final String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS `test`.`cache_table` (\n" +
+    private static final String TABLE_CHECK_SQL = "SELECT * FROM information_schema.tables WHERE table_schema = :table_schema AND table_name = :table_name LIMIT 1;";
+    private static final String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS `test`.`CACHE_TABLE` (\n" +
             "  `id`    INT         NOT NULL AUTO_INCREMENT,\n" +
             "  `CACHE_KEY`   VARCHAR(30) NOT NULL,\n" +
             "  `CACHE_VALUE` VARCHAR(50) NOT NULL,\n" +
             "  PRIMARY KEY (`id`)\n" +
             ");";
 
-    private static final String DELETE_DATA_SQL = "DELETE FROM `test`.`cache_table`\n" +
+    private static final String DELETE_DATA_SQL = "DELETE FROM `test`.`CACHE_TABLE`\n" +
             "WHERE id = '1';";
 
-    private static final String INSERT_DATA_SQL = "INSERT INTO `test`.`cache_table` (`id`, `CACHE_KEY`, `CACHE_VALUE`) VALUES (1, \"test\", \"test\");";
+    private static final String INSERT_DATA_SQL = "INSERT INTO `test`.`CACHE_TABLE` (`id`, `CACHE_KEY`, `CACHE_VALUE`) VALUES (1, \"test\", \"test\");";
 
     public CacheItem find(String key) {
+        initSchemaAndDataIfNecessary();
         Map<String, Object> param = new HashMap<String, Object>();
         param.put("cacheKey", key);
         return namedParameterJdbcTemplate.queryForObject(QUERY_SQL, param, new CacheItemMapper());
@@ -49,7 +50,6 @@ public class MySqlServiceManager {
 
     public class CacheItemMapper implements RowMapper<CacheItem> {
         public CacheItem mapRow(ResultSet resultSet, int i) throws SQLException {
-            initSchemaAndDataIfNecessary();
             CacheItem cacheItem = new CacheItem();
             cacheItem.setId(resultSet.getInt("id"));
             cacheItem.setKey(resultSet.getString("CACHE_KEY"));
